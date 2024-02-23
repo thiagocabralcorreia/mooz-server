0684e2ba83b2a46f0fe7c922e10cf6912b7fa78d
thiagocabralcorreia <thiagocabralcorreia@gmail.com>
2024-02-23 10:55:46 -0300

    ✨ feat: add logs and cors

---
+++ config/allowedOrigins.js
@@ -0,0 +1,7 @@
+const allowedOrigins = [
+  "http://localhost:3000",
+  "https://www.moozsn.com",
+  "https://moozsn.com",
+];
+
+module.exports = allowedOrigins;
--- server.js
+++ server.js
@@ -1,9 +1,23 @@
 const express = require("express");
 const app = express();
 const path = require("path");
+const { logger } = require("./middleware/logger");
+const errorHandler = require("./middleware/errorHandler");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+const corsOptions = require("./config/corsOptions");
 const PORT = process.env.PORT || 3333;
 
-app.use("/", express.static(path.join(__dirname, "./public")));
+app.use(logger);
+
+app.use(cors(corsOptions));
+
+app.use(express.json());
+
+app.use(cookieParser());
+
+app.use("/", express.static(path.join(__dirname, "public")));
+// app.use(express.static("public"));
 
 app.use("/", require("./routes/root"));
 
@@ -18,5 +32,7 @@ app.all("*", (req, res) => {
     res.type("txt").send("404 Not Found");
   }
 });
+
+app.use(errorHandler);
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
